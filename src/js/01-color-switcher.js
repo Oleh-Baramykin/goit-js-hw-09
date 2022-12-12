87ad0b40d8c230e34e6a2e5b0bfb7e3ddc9b7565
Oleh Baramykin <olegix7891@ukr.net>
2022-12-12 11:34:16 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+let timerId = null;
+
+const onClick = () => {
+  startBtn.setAttribute('disabled', null);
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const offClick = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+};
+
+startBtn.addEventListener('click', onClick);
+stopBtn.addEventListener('click', offClick);
